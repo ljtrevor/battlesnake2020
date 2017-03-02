1bd8ad1686d478f290f310eb773be89863a8f312
kyle.boyd <kyle.boyd@workday.com>
2017-03-01 19:13:48 -0800

    Override __eq__ on Point class

--- app/model/point.py
+++ app/model/point.py
@@ -2,3 +2,16 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+
+    def __eq__(self, point):
+        """Returns true if the provided point is equal to self. False otherwise.
+
+            Args:
+                Point
+
+            Return:
+                bool
+
+        """
+        return self.x == point.x and self.y == point.y
